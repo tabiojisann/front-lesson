2a256bccbb29350650e4445d21bc438e4a83d0ae
tabiojisann <takumiezumi@icloud.com>
2020-07-04 13:25:09 +0900

    プロパティ追加

---
+++ 04_JavaScriptの基礎を固める/050_オブジェクトの使い方/start/main.js
@@ -0,0 +1,32 @@
+const books = {
+    novel: {
+      name: ['火花', '赤毛のアン', '星の王子様']
+    },
+
+    comic: {
+      name: ['鬼滅の刃', 'ONE PIECE', 'キングダム']
+    },
+
+    business: {
+      name: ['7つの週間', 'サピエンス全史', '自助論']
+    },
+
+    philosophy: {
+      name: ['方法序説', '死に至る病', 'ニコマコス倫理学']
+    },
+
+    biography: {
+      name: ['フランクリン自伝', '果てなき野望', 'マイ・ストーリー']
+    },
+
+    favorite: {
+      name: ['FACTFULLNESS', '蹴りたい背中', '恋は雨上がりのように']
+    }
+}
+
+
+
+
+
+
+
