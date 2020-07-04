258aaba1f0dc144cb505dc084b50c51d93d0e70d
tabiojisann <takumiezumi@icloud.com>
2020-07-04 12:45:59 +0900

    up

--- 04_JavaScriptの基礎を固める/040_配列の使い方/start/main.js
+++ 04_JavaScriptの基礎を固める/040_配列の使い方/start/main.js
@@ -5,11 +5,11 @@ const person = {
   interests: {
       sports: 'soccer',
       music: 'piano'
+  },
+  getFullName: function() {
+    console.log(this.name[0] + this.name[1])
   }
 };
 
-console.log(person.name[]);
-
-
-
+person.getFullName();
 
