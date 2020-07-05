f1d665e64125f4e4be1a5e7cf359813d54989667
tabiojisann <takumiezumi@icloud.com>
2020-07-05 12:24:18 +0900

    ループ

---
+++ 04_JavaScriptの基礎を固める/060_ループの記述方法/start/main.js
@@ -0,0 +1,14 @@
+// const array = [1,2,3,4,5,9];
+
+
+// for(let i = 0; i < array.length; i = i + 1) {
+//   console.log(array[i]);
+// }
+
+
+const array = [1,3,5,7,9,11,13,15];
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
