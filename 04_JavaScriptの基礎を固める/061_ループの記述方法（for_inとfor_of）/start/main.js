6ff107f9bc5f0a275523fce78c3a9a3646eff241
tabiojisann <takumiezumi@icloud.com>
2020-07-06 22:29:22 +0900

    オブジェクト

--- 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
+++ 04_JavaScriptの基礎を固める/061_ループの記述方法（for_inとfor_of）/start/main.js
@@ -1,5 +1,8 @@
-const arry = [1, 2, 3, 4, 5, 6];
+const array = [1, 2, 3, 4, 5, 6];
 
-for (let i = 0; i < arry.length; i++) {
-    console.log(i);
+for (let i of array) {
+    let v = array[i];
+    console.log(v);
 }
+
+
