8b60ebc29c8dba4e58ac6127bfe5b4f410aae5d2
tabiojisann <takumiezumi@icloud.com>
2020-07-09 01:30:46 +0900

    callback

--- 04_JavaScriptの基礎を固める/074_コールバック関数とループ/start/main.js
+++ 04_JavaScriptの基礎を固める/074_コールバック関数とループ/start/main.js
@@ -1,12 +1,48 @@
 
-function roland(back) {
-  console.log(back());
+// function roland(back) {
+//   console.log(back());
+// }
+
+// roland( function() {
+//   return '俺か俺以外か';
+// })
+
+
+
+// const roland = function roland(){
+//   console.log('俺か,俺以外か');
+// }
+
+// roland();
+
+// function roland(callback) {
+//   console.log(callback());
+// }
+
+// // const remark = function() {
+// //   return '俺か俺以外か';
+// // }
+
+// roland( function() {
+//   return '俺か、俺以外か';
+// })
+
+// function roland(me, otherThanThat) {
+//   // console.log(me);
+//   console.log(me(otherThanThat))
+// }
+
+// roland( function(otherThanThat) {
+//   return '俺か' + otherThanThat;
+// }, '俺以外か');
+
+function roland(a, b, callback) {
+  const result = callback(a, b);
+  console.log(result);
 }
 
+function remark(a, b) {
+  return a + b;
+}
 
-
-
-roland( function() {
-  return '俺か俺以外か';
-})
-
+roland('俺か', '俺以外か', remark);
