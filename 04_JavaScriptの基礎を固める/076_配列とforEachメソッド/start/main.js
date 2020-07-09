36e6b293da6de886f283a38f5c216caf8d0445b7
tabiojisann <takumiezumi@icloud.com>
2020-07-09 20:04:33 +0900

    reduce

--- 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/076_配列とforEachメソッド/start/main.js
@@ -1,6 +1,23 @@
 const array = [1,2,3,4,5];
 
-array.forEach(function(val, i, ary) {
-  console.log(ary);
-})
+array.reduce(function(accu, curr) {
+  console.log(accu, curr);
+  return accu + curr;
+}, 10)
+
+const str = "animation";
+
+const strArray = str.split('');
+
+console.log(strArray);
+
+const result =  strArray.reduce((accu, curr) => {
+  return accu + '<' + curr + '>';
+}, "")
+
+console.log(result);
+
+
+
+
 
