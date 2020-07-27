7b047ac8a2079fd8b955b4ae90707b59e961da40
tabiojisann <takumiezumi@icloud.com>
2020-07-27 09:27:33 +0900

    fix

--- 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
+++ 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -1,6 +1,19 @@
 const child = document.querySelector('.child');
-const cb = function() {
-  alert('interesting');
+const cb = function(entries, observer) {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+          console.log('inview');
+          entry.target.classList.add('inview');
+      } else {
+          console.log('out view');
+          entry.target.classList.remove('inview');
+      }
+    });
 }
-const io = new IntersectionObserver(cb);
+const options = {
+  root: null,
+  rootMargin: "-300px 0px",
+  thereshold: [0, 0.5, 1]
+};
+const io = new IntersectionObserver(cb, options);
 io.observe(child);
